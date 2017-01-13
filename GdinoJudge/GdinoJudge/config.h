691649cc101fba73a832ac7e921169ec48b89dcd
shanxuan <781244184@qq.com>
2017-01-12 20:53:19 -0800

    项目目录分层

---
+++ GdinoJudge/GdinoJudge/config.h
@@ -0,0 +1,13 @@
+//
+//  config.h
+//  GdinoJudge
+//
+//  Created by csx on 16/12/17.
+//  Copyright © 2016年 csx. All rights reserved.
+//
+
+#ifndef config_h
+#define config_h
+
+
+#endif /* config_h */
